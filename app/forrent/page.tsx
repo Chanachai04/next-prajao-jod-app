0d5d58266d700e92d6c2de57e054fb4d9e9973b9
Bannaruj <s6652410030@sau.ac.th>
2025-11-01 22:52:50 +0700

    feat: update ForRent and ForRentDetail components with improved form fields and layout

--- app/forrent/page.tsx
+++ app/forrent/page.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Field, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function ForRent() {
   return (
     <>
       {/* --ส่วนหัว */}
-      <div className="min-h-screen px-4 md:px-10 lg:px-20 py-10">
-        <h1 className="text-2xl pt-10">ปล่อยเช่าที่จอดรถ</h1>
+      <div className="min-h-screen px-4 md:px-10 lg:px-20 py-5">
+        <h1 className="text-3xl pt-10">ปล่อยเช่าที่จอดรถ</h1>
         <p className="py-2">
           กรุณากรอกข้อมูลให้ครบถ้วนก่อนทำการปล่อยเช่าที่จอดรถ
         </p>
@@ -18,38 +19,70 @@ export default function ForRent() {
           <div className="flex gap-30">
             <Field>
               <FieldLabel htmlFor="name">ชื่อ *</FieldLabel>
-              <Input id="name" placeholder="" required />
+              <Input
+                id="name"
+                placeholder=""
+                required
+                className="border-2 border-gray-400"
+              />
             </Field>
             <Field>
               <FieldLabel htmlFor="surname">นามสกุล *</FieldLabel>
-              <Input id="surname" placeholder="" required />
+              <Input
+                id="surname"
+                placeholder=""
+                required
+                className="border-2 border-gray-400"
+              />
             </Field>
           </div>
           <div className="flex gap-30">
             <Field>
               <FieldLabel htmlFor="email">อีเมล *</FieldLabel>
-              <Input id="email" placeholder="" required />
+              <Input
+                id="email"
+                placeholder=""
+                required
+                className="border-2 border-gray-400"
+              />
             </Field>
             <Field>
               <FieldLabel htmlFor="name">รหัสประจำตัวประชาชน *</FieldLabel>
-              <Input id="id" placeholder="" required />
+              <Input
+                id="id"
+                placeholder=""
+                required
+                className="border-2 border-gray-400"
+              />
             </Field>
           </div>
           <div className="flex gap-30">
             <Field>
               <FieldLabel htmlFor="email">Line ID *</FieldLabel>
-              <Input id="lineid" placeholder="" required />
+              <Input
+                id="lineid"
+                placeholder=""
+                required
+                className="border-2 border-gray-400"
+              />
             </Field>
             <Field>
               <FieldLabel htmlFor="name">เบอร์โทรติดต่อ *</FieldLabel>
-              <Input id="phonenumber" placeholder="" required />
+              <Input
+                id="phonenumber"
+                placeholder=""
+                required
+                className="border-2 border-gray-400"
+              />
             </Field>
           </div>
         </div>
         {/* --ปุ่มบันทึก */}
         <div className="pt-7 flex justify-end">
           <Button className="w-full md:w-auto md:px-30">
-            <div className="text-lg">บันทึกข้อมูล</div>
+            <Link href="/forrentdetail">
+              <div className="text-lg font-light">บันทึกข้อมูล</div>
+            </Link>
           </Button>
         </div>
       </div>
--- app/forrentdetail/page.tsx
+++ app/forrentdetail/page.tsx
@@ -5,7 +5,6 @@ import {
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
@@ -16,10 +15,9 @@ export default function ForRentDetail() {
   return (
     <>
       {/* --ส่วนหัว */}
-      <div className="min-h-screen px-4 md:px-10 lg:px-20 py-10">
-        <h1 className="text-2xl pt-10">ปล่อยเช่าที่จอดรถ</h1>
+      <div className="min-h-screen px-4 md:px-10 lg:px-20 py-5">
+        <h1 className="text-3xl pt-10">ปล่อยเช่าที่จอดรถ</h1>
         <p className="py-2">ข้อมูลทั่วไปที่จอดรถ</p>
-
         {/* --ส่วน field */}
         <div className="flex flex-col space-y-6">
           {/* บรรทัด: ชื่อที่จอด + ประเภท */}
@@ -58,7 +56,6 @@ export default function ForRentDetail() {
               </Select>
             </div>
           </div>
-
           {/* คำบรรยาย */}
           <Field>
             <FieldLabel htmlFor="Description">คำบรรยาย</FieldLabel>
@@ -68,6 +65,65 @@ export default function ForRentDetail() {
               className="resize-none border-2 border-gray-400 w-full"
             />
           </Field>
+        </div>
+        {/* --จำนวน/บ้านเลขที่ */}
+        <div className="flex gap-10 pt-3">
+          <Field>
+            <FieldLabel htmlFor="name">จำนวนที่จอด (คัน) *</FieldLabel>
+            <Input
+              id="amout"
+              placeholder=""
+              required
+              className="border-2 border-gray-400"
+            />
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="surname">
+              ที่อยู่ (บ้านเลขที่ หมู่บ้าน ซอย ถนน) *
+            </FieldLabel>
+            <Input
+              id="้houseno"
+              placeholder=""
+              required
+              className="border-2 border-gray-400"
+            />
+          </Field>
+        </div>
+        {/* --แขวง/เขต */}
+        <div className="flex gap-10 pt-3">
+          <Field>
+            <FieldLabel htmlFor="name">แขวง / ตำบล *</FieldLabel>
+            <Input
+              id="district"
+              placeholder=""
+              required
+              className="border-2 border-gray-400"
+            />
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="surname">เขต / อำเภอ *</FieldLabel>
+            <Input
+              id="country"
+              placeholder=""
+              required
+              className="border-2 border-gray-400"
+            />
+          </Field>
+        </div>
+        <div className="flex gap-10 pt-3">
+          <Field>
+            <FieldLabel htmlFor="name">จุดสังเกต *</FieldLabel>
+            <Input
+              id="landmark"
+              placeholder="จะหาที่จอดรถคุณได้อย่างไร"
+              required
+              className="border-2 border-gray-400"
+            />
+          </Field>
+        </div>
+        {/* --checkbox */}
+        <div className="">
+          <div></div>
         </div>
       </div>
     </>
