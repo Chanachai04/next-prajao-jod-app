9bf54435e2112f686b4cd1ae5bc840d39ca77af6
Chanachai04 <chn.benmat@gmail.com>
2025-11-17 01:31:31 +0700

    feat: refactor login and registration components to use separate forms with suspense loading

---
+++ components/login/LoginForm.tsx
@@ -0,0 +1,146 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Eye, EyeOff, LockKeyhole, Mail } from "lucide-react";
+import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+export default function LoginForm() {
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(null);
+
+    // Validate email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email) {
+      setError("กรุณากรอกอีเมล");
+      return;
+    } else if (!emailRegex.test(email)) {
+      setError("รูปแบบอีเมลไม่ถูกต้อง");
+      return;
+    }
+
+    // Validate password
+    if (!password) {
+      setError("กรุณากรอกรหัสผ่าน");
+      return;
+    } else if (password.length < 6) {
+      setError("รหัสผ่านต้องมีอย่างน้อย 6 ตัวอักษร");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const res = await fetch("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await res.json();
+
+      if (res.ok && data.message === "Login success") {
+        window.dispatchEvent(new Event("loginStatusChanged"));
+
+        // ดึง path redirect จาก query ถ้าไม่มี default เป็น "/"
+        const redirectPath = searchParams.get("redirect") || "/";
+        router.push(redirectPath);
+      } else {
+        setError(data.message || "เข้าสู่ระบบไม่สำเร็จ");
+      }
+    } catch (err) {
+      console.error(err);
+      setError("อีเมลหรือรหัสผ่านไม่ถูกต้อง");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="border rounded-2xl p-10 shadow-2xl sm:w-[500px]">
+        <p className="text-4xl">เข้าสู่ระบบ</p>
+        <form onSubmit={handleSubmit}>
+          <div className="my-2">
+            <Label htmlFor="email" className="text-lg">
+              อีเมล
+            </Label>
+            <div className="relative mt-2">
+              <Mail className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500 w-6 h-6" />
+              <Input
+                id="email"
+                type="email"
+                placeholder="กรอกอีเมลของคุณ"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="pl-10 pr-10 w-full text-sm md:text-lg h-10"
+              />
+            </div>
+          </div>
+          <div>
+            <Label htmlFor="password" className="text-lg">
+              รหัสผ่าน
+            </Label>
+            <div className="relative mt-2">
+              <LockKeyhole className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500 w-6 h-6" />
+              <Input
+                id="password"
+                type={isShowPassword ? "text" : "password"}
+                placeholder="กรอกรหัสผ่านของคุณ"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="pl-10 pr-10 w-full text-sm md:text-lg h-10"
+              />
+              <button
+                type="button"
+                className="cursor-pointer absolute right-2 top-1/2 -translate-y-1/2 text-gray-500"
+                onClick={() => setIsShowPassword(!isShowPassword)}
+              >
+                {isShowPassword ? (
+                  <EyeOff className="w-6 h-6" />
+                ) : (
+                  <Eye className="w-6 h-6" />
+                )}
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-end my-2">
+            <Link
+              href="/forgotpassword"
+              className="hover:underline hover:text-blue-600"
+            >
+              ลืมรหัสผ่าน?
+            </Link>
+          </div>
+
+          {error && <div className="text-red-600 my-2 text-sm">{error}</div>}
+
+          <Button
+            className="my-2 w-full text-lg py-5 cursor-pointer"
+            disabled={loading}
+          >
+            {loading ? "กำลังเข้าสู่ระบบ..." : "เข้าสู่ระบบ"}
+          </Button>
+
+          <div className="my-2">
+            ยังไม่ได้เป็นสมาชิก?{" "}
+            <Link href="/register" className="hover:underline text-blue-600">
+              สมัครเลยตอนนี้
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
